38afed327d922a089d5bee9d86281ac7c2f6a0e6
Lacromisss <tu7a45yc6@code.edu.az>
2022-03-17 04:27:56 +0400

    Add project files.

---
+++ ConsoleApp30/Group.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp30
+{
+    internal class Group
+    {
+        public Group(string noo, string studentlimittt)
+        {
+            this._no = noo;
+            this._studentlimit = studentlimittt;
+        }
+
+        private string __no;
+        private string _password;
+
+        public string _no
+        {
+            get => this.__no;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Length==5)
+                    this.__no = value;
+            }
+        }
+        public string _studentlimit
+        {
+            get => this._studentlimit;
+            set
+            {
+                if (HasDigit1(value))
+                    this._studentlimit = value;
+            }
+        }
+
+
+
+
+
+        public bool HasUpper(string str)
+        {
+            int count = 0;
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsUpper(str[i]))
+                        count++;
+                }
+                if (count==2)
+                {
+                    return true;
+
+                }
+            }
+
+            return false;
+        }
+        public bool Hasnumber(string str)
+        {
+            int count = 0;
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsDigit (str[i]))
+                        count++;
+                }
+                if (count == 3)
+                {
+                    return true;
+
+                }
+
+            }
+
+            return false;
+        }
+        public bool HasDigit1(string str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsDigit(str[i]))
+                        return true;
+                }
+            }
+
+
+            return false;
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    }
+
+    
+
+    
+    
+    
+    
+    
+
---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ne qeder list olsun?");
+            int asd= int.Parse( Console.ReadLine());
+            Group[] grup = new Group[asd];
+            for (int i = 0; i < asd; i++)
+            {
+                string inputStrStr;
+                do
+                {
+                    Console.WriteLine("grup No");
+                    inputStrStr = Console.ReadLine();
+
+
+                } while (string.IsNullOrWhiteSpace(inputStrStr) || inputStrStr.Length != 5 || !Hasnumber(inputStrStr) || !HasUpper(inputStrStr));
+                string intput;
+                do
+                {
+                    Console.WriteLine("groupdaki telebelerin max sayi");
+                    intput = Console.ReadLine();
+
+                } while (string.IsNullOrWhiteSpace(intput) || !HasDigit1(intput));
+             
+
+
+            }
+
+            
+           
+            
+            
+            
+           
+        }
+        static bool HasUpper(string str)
+        {
+            int count = 0;
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsUpper(str[i]))
+                        count++;
+                }
+                if (count == 2)
+                {
+                    return true;
+
+                }
+            }
+
+            return false;
+        }
+        static bool Hasnumber(string str)
+        {
+            int count = 0;
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsDigit(str[i]))
+                        count++;
+                }
+                if (count == 3)
+                {
+                    return true;
+
+                }
+
+            }
+
+            return false;
+        }
+        static bool HasDigit1(string str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsDigit(str[i]))
+                        return true;
+                }
+            }
+
+
+            return false;
+        }
+        
+
+
+
+
+    }
+}
